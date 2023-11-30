70098c4b5fb30ea145d5c5c0f8e67f7b0c030e71
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-01 01:08:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
-resource "aws_instance" "master" {
+resource "aws_instance" "Master-Instance" {
   ami           = "ami-03caf91bb3d81b843" 
   instance_type = "t2.micro"
   tags = {
