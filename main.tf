2b0f4e52a43794428b92d3a575de632f96142038
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-01 22:49:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,24 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
-// To Generate Private Key
-resource "tls_private_key" "rsa_4096" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
 
 resource "aws_instance" "Master-Instance" {
   ami           = "ami-03caf91bb3d81b843" 
   instance_type = "t2.micro"
+
+  key_name = aws_key_pair.example_key.key_name
+
   tags = {
       Name = "Master-Instance"
   }
+}
+
+resource "aws_key_pair" "example_key" {
+  key_name   = "example-key"
+  public_key = tls_private_key.example_key.public_key_openssh
+}
+
+resource "tls_private_key" "example_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
