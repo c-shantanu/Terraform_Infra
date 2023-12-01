770161f02a08052a72d24f447af0759cfa162bbf
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-01 23:19:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,14 +14,18 @@ resource "aws_instance" "Master-Instance" {
   }
 }
 
-resource "aws_key_pair" "example_key" {
-  key_name   = "example-key"
-  public_key = tls_private_key.example_key.public_key_openssh
+resource "aws_key_pair" "key-pair-01" {
+  key_name   = example_KP
+  public_key = tls_private_key.rsa.public_key_openssh
 }
-
-resource "tls_private_key" "example_key" {
+resource "tls_private_key" "rsa" {
   algorithm = "RSA"
-  rsa_bits  = 2048
-  file_permission = "0400"
-
+  rsa_bits  = 4096
 }
+resource "local_file" "key-01" {
+  content         = tls_private_key.rsa.private_key_pem
+  file_permission = "0400"
+  filename        = example_KP
+}
+
+
