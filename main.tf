fb11a9af90f11bcaff022bc4baebd7dab842fc9a
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-01 23:04:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,10 +17,11 @@ resource "aws_instance" "Master-Instance" {
 resource "aws_key_pair" "example_key" {
   key_name   = "example-key"
   public_key = tls_private_key.example_key.public_key_openssh
-  file_permission = "0400"
 }
 
 resource "tls_private_key" "example_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
+  file_permission = "0400"
+
 }
