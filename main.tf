ba591a0ad6e28d0b67f2d50eb8ba95b33fa292a3
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-01 22:56:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ resource "aws_instance" "Master-Instance" {
   ami           = "ami-03caf91bb3d81b843" 
   instance_type = "t2.micro"
 
-  #key_name = aws_key_pair.example_key.key_name
+  key_name = aws_key_pair.example_key.key_name
 
   tags = {
       Name = "Master-Instance"
   }
 }
 
-#resource "aws_key_pair" "example_key" {
-  #key_name   = "example-key"
-  #public_key = tls_private_key.example_key.public_key_openssh
-#}
+resource "aws_key_pair" "example_key" {
+  key_name   = "example-key"
+  public_key = tls_private_key.example_key.public_key_openssh
+}
 
-#resource "tls_private_key" "example_key" {
-  #algorithm = "RSA"
-  #rsa_bits  = 2048
-#}
+resource "tls_private_key" "example_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
