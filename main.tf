8d21a13bdfb86ec74931c5692e11915e8d34b74b
Shantanu <52127255+c-shantanu@users.noreply.github.com>
2023-12-01 22:42:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
+// To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_instance" "Master-Instance" {
   ami           = "ami-03caf91bb3d81b843" 
   instance_type = "t2.micro"
